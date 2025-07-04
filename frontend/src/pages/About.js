26fff52800bba8fcf7599fd2dbf2419d740f29c3
harnithlokesh <harnithmarshmello@gmail.com>
2025-07-04 11:29:51 +0530

    Initial commit

---
+++ frontend/src/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div>
+      <h1>About secureTitan</h1>
+      <p>We are building the future of cybersecurity education and consultancy services.</p>
+    </div>
+  );
+};
+
+export default About;
